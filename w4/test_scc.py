16a57679edeaeb8bc32c55c5aa1286cf2e4b5888
Thang Nguyen <thangnv2212@gmail.com>
2016-11-11 00:48:04 +0200

    test assert

--- w4/test_scc.py
+++ w4/test_scc.py
@@ -19,14 +19,14 @@ class TestSCC(unittest.TestCase):
     def test_scc(self):
         d = defaultdict(list, {0: [2,3], 1: [0,], 2:[1,], 3:[4,]})
         g1 = scc.Graph(5, d)
-        print g1.scc()
+        self.assertEqual(g1.scc(), [3,1,1])
 
         g2 = scc.Graph(10)
         lines = test2.split("\n")
         for l in lines:
             arr = l.split()
             g2.add_edge(int(arr[0]), int(arr[1]))
-        print g2.scc()
+        self.assertEqual(g2.scc(), [3,3,3,1])
 
 if __name__ == "__main__":
     unittest.main()

6b5fcee74eb4071da0de98d6d27548c2655cf326
Thang Nguyen <thangnv2212@gmail.com>
2016-12-25 22:04:55 +0200

    add path reconstruction

--- w9/mwis.py
+++ w9/mwis.py
@@ -8,15 +8,23 @@ def mwis(arr):
     n = len(arr)
 
     w = [0] * (n+1)
-    path = [0] * n
-
     w[1] = arr[0] # first vertex is chosen defautly
-    path[0] = 1 # first vertex is chosen defautly
 
     for i in range(2, n+1):
-        # without reconstructing the path
         w[i] = max(w[i-1], w[i-2] + arr[i-1])
 
+    # reconstruct the path
+    path = []
+    i = n
+    while i > 0:
+        if w[i] != w[i-1]:
+            path.append(arr[i-1])
+            i -= 2
+        else:
+            i -= 1
+
+    path.reverse()
+    print "Path: %s" % path
     print "Weight: %s \n" % w[n]
 
     return w[n]
